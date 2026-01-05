9b6b8ed968716ea6c8072cc9e2cac824236a22c2
Vijit Bhargav <99094378+vijitb@users.noreply.github.com>
2026-01-05 18:52:07 +0530

    Update whatsapp_webhook.py
    Updated for handling and storing Image in s3 bucket + text in dynamodb table.
    DynamoDB table is also updated to include image_urls

--- lambda/whatsapp_webhook.py
+++ lambda/whatsapp_webhook.py
@@ -3,12 +3,25 @@ import base64
 import uuid
 import hashlib
 import boto3
+import requests
+import os
 from datetime import datetime
 from urllib.parse import parse_qs
 from twilio.twiml.messaging_response import MessagingResponse
 
+# initialize dynamodb client
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('waste_reports')
+
+# initialize s3 client
+s3 = boto3.client("s3")
+BUCKET_NAME = "waste-report-images"
+
+# initialize environment variables
+# Store your twillio token and account id in environment variables
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+
 
 def lambda_handler(event, context):
 
@@ -31,6 +44,27 @@ def lambda_handler(event, context):
 
     print("Incoming WhatsApp data:", data)
 
+    # image capturing logic
+        # Twilio Media Payload
+        # 1. NumMedia (no. of images)
+        # 2. MediaUrl0, MediaUrl1, MediaUrl2, ... (image url)
+        # 3. MediaContentType0, MediaContentType1, MediaContentType2, ... (image/jpeg)
+    try:
+        num_media = int(data.get("NumMedia", [0])[0])
+        images_urls = []
+
+        if num_media > 0:
+            for i in range(num_media):
+                media_url = data.get(f"MediaUrl{i}", [""])[0]
+                media_content_type = data.get(f"MediaContentType{i}", [""])[0]
+
+                if media_content_type.startswith("image"):
+                    image_bytes = download_twilio_media(media_url)
+                    s3_path = upload_to_s3(image_bytes, report_id)
+                    images_urls.append(s3_path)
+    except Exception as e:
+        print("Error processing image:", str(e))
+
     resp = MessagingResponse()
     resp.message("Thank you for reporting the incident.")
 
@@ -41,7 +75,8 @@ def lambda_handler(event, context):
         "description": message,
         "status": "open",
         "source": "whatsapp",
-        "reporter_hash": reporter_hash
+        "reporter_hash": reporter_hash,
+        "images_urls": images_urls
     }
 
     table.put_item(Item=item)
@@ -56,3 +91,26 @@ def lambda_handler(event, context):
         },
         "body": str(resp)
     }
+
+
+def download_twilio_media(media_url):
+    response = requests.get(
+        media_url,
+        auth=(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    )
+
+    response.raise_for_status() # prevents silent failures
+    return response.content
+
+
+def upload_to_s3(image_bytes, report_id):
+    key = f"reports/{report_id}.jpg"
+    
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=key,
+        Body=image_bytes,
+        ContentType="image/jpeg"
+    )
+    
+    return f"s3://{BUCKET_NAME}/{key}"
